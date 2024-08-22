3a615de3c48d84643d0b81aa3f26c462072bd8b6
Matheus Passatuto <matheus.passatuto@bkbank.com.br>
2024-08-22 17:40:58 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace ConjuntosCursos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<int> curso1 = new HashSet<int>();
+            HashSet<int> curso2 = new HashSet<int>();
+            HashSet<int> curso3 = new HashSet<int>();
+
+            Console.Write("Quantos estudantes no Curso1?");
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+               int id = int.Parse(Console.ReadLine());
+               curso1.Add(id);               
+            }
+            Console.Write("Quantos estudantes no Curso2?");
+            n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                int id = int.Parse(Console.ReadLine());
+                curso2.Add(id);
+            }
+            Console.Write("Quantos estudantes no Curso3?");
+            n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                int id = int.Parse(Console.ReadLine());
+                curso3.Add(id);
+            }
+
+            HashSet<int> list = new HashSet<int>(curso1);
+            list.UnionWith(curso2);
+            list.UnionWith(curso3);
+            Console.WriteLine("Total de estudantes: " + list.Count);
+        }
+    }
+}
